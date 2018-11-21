41622b59b301f67300c3d14d6cff362b9ad440ec
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2018-11-21 18:05:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -27,5 +27,19 @@ function init() {
 	
 		stage.addChild(sprite);
 		}
+
+console.log(new Date());
+console.log('Dude!');
+sleep(1000);
+console.log(new Date());
 renderer1.render(stage);
 console.log("runOK");
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
